99603a1fc2a55e4043e48d8901f59a54d5ecfd4e
MlsDmitry <molokovich.dima@gmail.com>
2021-04-07 23:58:22 +0300

    post form

---
+++ app/forms/post_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from sqlalchemy import orm
+from wtforms import StringField, BooleanField, SubmitField, IntegerField, TextAreaField
+from wtforms.validators import DataRequired
+
+class PostForm(FlaskForm):
+    title = StringField('Заголовок')
+    description = TextAreaField('Описание товара', validators=[DataRequired()])
+
+    tags = StringField('Теги')
+    submit = SubmitField('Добавить')
+    
